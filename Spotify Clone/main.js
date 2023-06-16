19fd99c9175df7cc0fa75267711cfe07fcae19f8
madhurima999 <gorainmadhurima999@gmail.com>
2023-06-16 13:33:58 +0530

    p

--- Spotify Clone/main.js
+++ Spotify Clone/main.js
@@ -65,7 +65,8 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{ 
         makeAllPlays();
         songIndex = parseInt(e.target.id);
-        e.target.classList.remove('fa-play-circle');
+        if(audioElement.paused)
+       { e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
         audioElement.src = `songs/${songIndex+1}.mp3`;
         masterSongName.innerText = songs[songIndex].songName;
@@ -73,7 +74,13 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         audioElement.play();
         gif.style.opacity = 1;
         masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.add('fa-pause-circle');}
+        else{
+           audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;  
+        }
     })
 })
 
